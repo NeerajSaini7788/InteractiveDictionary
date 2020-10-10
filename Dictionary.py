3340e4517f8a731ad8632e86d5c1475fffccbcfd
Neeraj Saini <asaini7788@gmail.com>
2020-10-09 21:42:44 -0400

    Fixed Word Suggestion

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,6 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
 #Input function
@@ -14,7 +16,22 @@ def translate(word):
     elif word.upper() in data.keys():
         return data[word.upper()]
     else:
-        return("Enable to find word")
+        return(possible_nearest(word.lower()))
+
+
+
+
+def possible_nearest(word):
+    closest_word = get_close_matches(word , data.keys())
+    if (len(closest_word) > 0):
+            choice  = input("Did you mean " + closest_word[0] +" instead?" + "\nEnter Y or N: ")
+            if choice == "Y":
+                return data[closest_word[0]]
+            elif choice == "N":
+                return("No Such Word found. Try again!!")
+    else:
+        return ("No such word. Try again!!")
+
 
 
 while(1):
